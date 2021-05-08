2f3d795de992e976863f8f57c279aee776d76683
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-05-09 02:06:00 +0530

    inintial commit with static pages

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log("Static Ghpages CD demo!!!");
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./public/index.html",
+          to: `${__dirname}/dist/index.html`,
+        },
+        {
+          from: "./public/vader.css",
+          to: `${__dirname}/dist/vader.css`,
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new HtmlMinimizerPlugin(),
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "bundle.js",
+  },
+  mode: "production",
+  module: {},
+};
